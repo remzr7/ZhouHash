35635bff106f176296cbdfd5d49d98af91d5ffac
remzr7 <remzr7@gmail.com>
2015-03-06 11:58:29 +0800

    +WebView Fix

---
+++ DBSHack/WebViewController.h
@@ -0,0 +1,13 @@
+//
+//  WebViewController.h
+//  DBSHack
+//
+//  Created by Rameez Remsudeen  on 3/6/15.
+//  Copyright (c) 2015 Rameez Remsudeen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WebViewController : UIViewController
+
+@end
---
+++ DBSHack/WebViewController.m
@@ -0,0 +1,55 @@
+//
+//  WebViewController.m
+//  DBSHack
+//
+//  Created by Rameez Remsudeen  on 3/6/15.
+//  Copyright (c) 2015 Rameez Remsudeen. All rights reserved.
+//
+
+#import "WebViewController.h"
+
+@interface WebViewController ()
+@property (weak, nonatomic) IBOutlet UIWebView *WebView;
+
+@end
+
+@implementation WebViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    [_WebView setDelegate:self];
+    
+    NSString *path = [[NSBundle mainBundle] bundlePath];
+    NSURL *baseURL = [NSURL fileURLWithPath:path];
+    
+    NSURL * url = [NSURL URLWithString:@"http://f.cl.ly/items/1l2i0x1C0F3l2I15092k/Bitmap%202.png"];
+    
+//    [_WebView loadRequest:[NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalAndRemoteCacheData timeoutInterval:4] ];
+    
+    [_WebView loadHTMLString:@"<img src='Jnnrm.png'>" baseURL:url];
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+-(UIStatusBarStyle)preferredStatusBarStyle
+{
+    return UIStatusBarStyleLightContent;
+}
+
+@end
